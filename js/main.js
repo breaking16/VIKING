436839370c032cfb67dd8e36e0f72809c53fb059
breaking16 <w.w804.162@gmail.com>
2024-01-22 12:05:00 +0200

    4half APARTY

--- js/main.js
+++ js/main.js
@@ -7,6 +7,13 @@ $(function () {
     }
   });
 
+  $(function (){
+   $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active ')
+   }) 
+    
+  });
+
   $('.heroes__slider-img').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
